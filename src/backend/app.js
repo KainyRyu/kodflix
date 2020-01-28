b62a1e2bf24ed02e8d20ca34f1208c01f68ba78f
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-28 16:55:38 +0000

    can't return the list of datas

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const DataB = require("./list");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send(DataB.Data));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
