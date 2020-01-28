919dd6adb2d631931e168d8b779b0c4ec78a117a
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-28 19:01:17 +0000

    Checking if Nodemon is working

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,7 +4,8 @@ const app = express();
 const port = 3001;
 // const Data = require("./list");
 
-app.get("/", (req, res) => res.send("Hello worldddddddd"));
+
+app.get("/", (req, res) => res.send("Hello Kodiri!"));
 app.get("/rest/shows", (req, res) => res.send(shows));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
