08f6c6cf48d4bf42ef7f59fe0e0f5225c5b50eb4
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-28 18:43:21 +0000

    ModeMon Works

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3001;
 // const Data = require("./list");
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello worldddddddd"));
 app.get("/rest/shows", (req, res) => res.send(shows));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
