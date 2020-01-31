f53518e573a3fda1e75f9095d14e18bc3fcf6013
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-31 12:40:48 +0000

    Connect frontend and backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const shows = require("./list");
+const getFilms = require("./list");
 const app = express();
 const port = 3001;
-// const Data = require("./list");
-
 
 app.get("/", (req, res) => res.send("Hello Kodiri!"));
-app.get("/rest/shows", (req, res) => res.send(shows));
+
+app.get("/rest/film/:id", (req, res) => {
+    let films = getFilms;
+    let film = films.find(film => film.id === req.params.id);
+    res.send(film.synopsis);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
