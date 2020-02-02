2fde9f968f2f50a8c92ea8dec401677aab6f5ee0
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-02 09:56:36 +0000

    return the list of data

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,10 +5,8 @@ const port = 3001;
 
 app.get("/", (req, res) => res.send("Hello Kodiri!"));
 
-app.get("/rest/film/:id", (req, res) => {
-    let films = getFilms;
-    let film = films.find(film => film.id === req.params.id);
-    res.send(film.synopsis);
+app.get("/rest/film/:id?", (req, res) => {
+    res.send(getFilms(req.params.id));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
