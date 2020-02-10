88c3b2c45689cdfddbef66f7f3e88c1d37f37881
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-10 20:24:34 +0000

    Returning the appropriate data on the page

--- src/frontend/cinema.js
+++ src/frontend/cinema.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import FilmCover from "./filmcover";
 import FilmDatas from "./filmDatas";
 
--- src/frontend/detailPage.js
+++ src/frontend/detailPage.js
@@ -1,116 +1,42 @@
-import React, { Component, useEffect, useState } from "react";
-import FilmDatas from "./filmDatas";
-import { Redirect } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import FilmDatas from "./filmDatas";
 import "./film.css";
 
-export default function FilmDetails() {
-    useEffect(() => {
-        fetchItems();
-    }, []);
+export default function FilmDetails(props) {
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-    const [filmDatas, setItems] = useState([]);
+  const [filmDatas, setFilmDatas] = useState([]);
 
-    const fetchItems = async () => {
-        const data = await fetch("/rest/films/");
-        const filmDatas = await data.json();
-        console.log(filmDatas)
-        setItems(filmDatas);
-    };
-
-    return filmDatas ? (
-        <div id="detailBg">
+  const fetchItems = async () => {
+    await fetch("/rest/films/")
+      .then(res => {
+        if (!res.status) throw new Error("Something went wrong!");
+        return res.json();
+      })
+      .then(res => setFilmDatas(res))
+      .catch(err => console.log(err));
+  };
+  const film = filmDatas.find(filmData => filmData.id === props.match.params.cinema);
+  console.log(film)
+  return film ? (
+    <div id="detailBg">
+        <>
           <div id="details">
-            <h1 className="filmTitle">{filmDatas.title}</h1>
-            <label>{filmDatas.detail}</label>
-            <p>{filmDatas.synopsis}</p>
+            <h1 className="filmTitle">{film.title}</h1>
+            <label>{film.detail}</label>
+            <p>{film.synopsis}</p>
           </div>
           <div id="divForImage">
-            <img
-              id="detailImg"
-              src={filmDatas.detailImg}
-              alt={filmDatas.id}
-            />
-            
+            {/* <img id="detailImg" src={film.detailImg} alt={film.id} /> */}
+
             <div className="detailOverlay"></div>
           </div>
-        </div>
-    ) : <Redirect to="/not-found" />
+        </>
+    </div>
+  ) : (
+    // <Redirect to="/not-found" />
+    <div>Pick a movie!</div>
+  );
 }
-
-
-
-
-// import React, { Component } from "react";
-// import FilmDatas from "./filmDatas";
-// import { Redirect } from "react-router-dom";
-
-// import "./film.css";
-
-// export default class FilmDetails extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       filmDatas: {}
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch("/rest/films/")
-//       .then(response => response.json()) //promises
-//       .then(filmDatas => {
-//         console.log("hello", filmDatas)
-//         this.setState({ filmDatas }); // when the key and value are the same
-//         // filmDatas: filmDatas
-//       });
-//   }
-
-//   render() {
-//     let datas = this.state.filmDatas
-//     return datas ? (
-//       <div>
-//         <div id="detailBg">
-//           <div id="details">
-//             <h1 className="filmTitle">{this.state.filmDatas.title}</h1>
-//             <label>{this.state.filmDatas.detail}</label>
-//             <p>{this.state.filmDatas.synopsis}</p>
-//             {console.log(this.state.filmDatas[0]) && this.state.filmData[0].id}
-//           </div>
-//           <div id="divForImage">
-//             <img
-//               id="detailImg"
-//               src={this.state.filmDatas.detailImg}
-//               alt={this.state.filmDatas.id}
-//             />
-            
-//             <div className="detailOverlay"></div>
-//           </div>
-//         </div>
-//       </div>
-//     ) : (
-//       <Redirect to="/not-found" />
-//     );
-
-
-//     //   <div>{<h1 className="filmTitle">{this.state.filmDatas.title}</h1>}</div>
-//   }
-// }
-
-// function FilmDatas(props) {
-//     return(
-//     <div>
-//         <Route exact path="/{props.title}" component={props.title}>
-//             <div className='detail'>
-//                 <div>
-//                     <h1>{props.title}</h1>
-//                     <h3>{props.rating}/10</h3>
-//                     <label>{props.details}</label>
-//                 </div>
-//                 <div>
-//                     <img src={props.image} alt={props.title}/>
-//                 </div>
-
-//             </div>
-//         </Route>
-//     </div>
-//     )
-// }
