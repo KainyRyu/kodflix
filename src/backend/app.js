eebdcba58c34796d774a79ad082cb7f22799797c
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-17 18:53:49 +0000

    trying to solve an error 1

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const app = express();
 const path = require('path');
 const getFilms = require("./filmDatas");
-const app = express();
 const port = process.env.PORT || 3001;
 
 // app.get("/", (req, res) => res.send("Hello Kodiri!"));
 app.get("/films/", (req, res) => { res.send(getFilms()); });
 app.use(express.static(path.join(__dirname, '../../build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
